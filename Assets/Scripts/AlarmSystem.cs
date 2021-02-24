77720711f2a9579ad7a7bba006f9c1246c776ab6
NeverUNICAL <kesarev0@mail.ru>
2021-02-24 12:51:19 +0300

    init

---
+++ Assets/Scripts/AlarmSystem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmSystem : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        var alarmOn = StartCoroutine(AlarmSystemOn());
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        var alarmOff = StartCoroutine(AlarmSystemOff());
+    }
+
+    private IEnumerator AlarmSystemOn()
+    {
+        _audioSource.volume = 0.1F;
+        _audioSource.Play();
+        for (int i = 0; i < 5; i++)
+        {
+            _audioSource.volume += 0.05F ;
+            yield return new WaitForSeconds(1F);
+        }
+    }
+    
+    private IEnumerator AlarmSystemOff()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            _audioSource.volume -= 0.05F ;
+            yield return new WaitForSeconds(0.5F);
+        }
+        _audioSource.Stop();
+    }
+    
+    
+}
---
+++ Assets/Scripts/ThiefMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Rigidbody2D _rigidbody2D;   
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(_speed*Time.deltaTime,0,0);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed*Time.deltaTime*-1,0,0);
+        }
+    }
+}
